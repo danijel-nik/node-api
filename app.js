1e973ac087e24262b0a2137427407535026c6994
danijel_nik <danijel.nikolic@ymail.com>
2020-11-21 11:49:24 +0100

    some functionality

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
 connectDB();
 
-const app = express();
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
+
+// Fix Cors issue on frontend
+app.use(cors());
 
 // Import Routes
 const postsRoute = require('./routes/posts');
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts!');
+// Submit a post
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
-router.get('/specific', (req, res) => {
-    res.send('Specific post!');
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+        // Returns posts
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Get the specific post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId)
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Delete post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Update post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId }, 
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
